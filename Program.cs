92d33d38338688018b445b4047a6deba63ddc1a6
Daniela Duarte <danielatavaresd@gmail.com>
2024-02-01 19:59:25 +0000

    CFB25.2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace aula25._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = 30;
+
+            dobrar(ref num);
+
+            Console.WriteLine(num);
+        }
+
+        static void dobrar(ref int valor)
+        {
+            valor *= 3;
+        }
+    }
+}
